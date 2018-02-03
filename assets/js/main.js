28b86ad3e2b19c6f2bc3fd7ca81a47378079cdd6
LAPTOP-8CFJ92M4\34976 <349763031@qq.com>
2018-02-03 23:10:08 +0800

    add

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,3 +20,15 @@ $(document).ready(function() {
   });
 
 });
+
+
+$('.top').click(function() {
+  $('html, body').stop().animate({scrollTop: 0}, 'slow', 'swing');
+});
+$(window).scroll(function() {
+  if ($(this).scrollTop() > $(window).height()) {
+    $('.top').addClass("up");
+  } else {
+    $('.top').removeClass("up");
+  }
+});

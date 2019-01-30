b95393893084965d33607f984308773a90860d83
leach.chen <leach>
2019-01-30 11:25:54 +0800

    add

---
+++ comments.php
@@ -0,0 +1,95 @@
+<?php
+/**
+ * The template for displaying comments.
+ *
+ * The area of the page that contains both current comments
+ * and the comment form.
+ *
+ * @package parallax-one
+ */
+
+/*
+ * If the current post is protected by a password and
+ * the visitor has not yet entered the password we will
+ * return early without loading the comments.
+ */
+if ( post_password_required() ) {
+	return;
+}
+?>
+<?php parallax_hook_comments_before(); ?>
+<div id="comments" class="comments-area">
+	<?php parallax_hook_comments_top(); ?>
+	<?php // You can start editing here -- including this comment! ?>
+
+	<?php if ( have_comments() ) : ?>
+		<h2 class="comments-title">
+			<?php
+			$comments_number = get_comments_number();
+			if ( 1 === $comments_number ) {
+				/* translators: %s: post title */
+				printf( _x( 'One thought on &ldquo;%s&rdquo;', 'comments title', 'parallax-one' ), get_the_title() );
+			} else {
+				printf(
+					/* translators: 1: number of comments, 2: post title */
+					_nx(
+						'%1$s thought on &ldquo;%2$s&rdquo;',
+						'%1$s thoughts on &ldquo;%2$s&rdquo;',
+						$comments_number,
+						'comments title',
+						'parallax-one'
+					),
+					number_format_i18n( $comments_number ),
+					get_the_title()
+				);
+			}
+			?>
+		</h2>
+
+		<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // are there comments to navigate through ?>
+		<nav id="comment-nav-above" class="navigation comment-navigation" role="navigation">
+			<h2 class="screen-reader-text"><?php esc_html_e( 'Comment navigation', 'parallax-one' ); ?></h2>
+			<div class="nav-links">
+
+				<div class="nav-previous"><?php previous_comments_link( esc_html__( 'Older Comments', 'parallax-one' ) ); ?></div>
+				<div class="nav-next"><?php next_comments_link( esc_html__( 'Newer Comments', 'parallax-one' ) ); ?></div>
+
+			</div><!-- .nav-links -->
+		</nav><!-- #comment-nav-above -->
+		<?php endif; // check for comment navigation ?>
+
+		<ol class="comment-list">
+			<?php
+				wp_list_comments( array(
+					'style'       => 'ol',
+					'short_ping'  => true,
+					'avatar_size' => 60,
+				) );
+			?>
+		</ol><!-- .comment-list -->
+
+		<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // are there comments to navigate through ?>
+		<nav id="comment-nav-below" class="navigation comment-navigation" role="navigation">
+			<h2 class="screen-reader-text"><?php esc_html_e( 'Comment navigation', 'parallax-one' ); ?></h2>
+			<div class="nav-links">
+
+				<div class="nav-previous"><?php previous_comments_link( esc_html__( 'Older Comments', 'parallax-one' ) ); ?></div>
+				<div class="nav-next"><?php next_comments_link( esc_html__( 'Newer Comments', 'parallax-one' ) ); ?></div>
+
+			</div><!-- .nav-links -->
+		</nav><!-- #comment-nav-below -->
+		<?php endif; // check for comment navigation ?>
+
+	<?php endif; // have_comments() ?>
+
+	<?php
+		// If comments are closed and there are comments, let's leave a little note, shall we?
+	if ( ! comments_open() && '0' != get_comments_number() && post_type_supports( get_post_type(), 'comments' ) ) :
+	?>
+	<p class="no-comments"><?php esc_html_e( 'Comments are closed.', 'parallax-one' ); ?></p>
+	<?php endif; ?>
+
+	<?php comment_form(); ?>
+	<?php parallax_hook_comments_bottom(); ?>
+</div><!-- #comments -->
+<?php parallax_hook_comments_after(); ?>
---
+++ content-contact.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Contact template content.
+ *
+ * @package parallax-one
+ */
+?>
+<article id="post-<?php the_ID(); ?>" <?php post_class( 'contact-page' ); ?>>
+
+	<div class="container">
+
+		<header class="entry-header">
+			<?php the_title( '<h1 class="entry-title single-title">', '</h1>' ); ?>
+			<div class="colored-line-left"></div>
+			<div class="clearfix"></div>
+		</header><!-- .entry-header -->
+
+		<div class="entry-content content-page parallax_one_contact_form">
+
+			<?php
+				$parallax_one_contact_form_shortcode = get_theme_mod( 'parallax_one_contact_form_shortcode' );
+
+				$contact_column = empty( $parallax_one_contact_form_shortcode ) || $post->post_content === '' ? 'col-md-12' : 'col-md-6';
+
+			if ( $post->post_content !== '' ) :
+				echo '<div class="' . $contact_column . '">';
+				the_content();
+				echo '</div>';
+				endif;
+
+			if ( ! empty( $parallax_one_contact_form_shortcode ) ) {
+				echo '<div class="' . $contact_column . '">';
+				echo do_shortcode( $parallax_one_contact_form_shortcode );
+				echo '</div>';
+			}
+			?>
+
+			<footer class="entry-footer">
+				<?php edit_post_link( esc_html__( 'Edit', 'parallax-one' ), '<span class="edit-link">', '</span>' ); ?>
+			</footer><!-- .fentry-footer -->
+
+		</div><!-- .entry-content -->
+
+	</div>
+
+	
+		<?php
+			$parallax_one_contact_map_shortcode = get_theme_mod( 'parallax_one_contact_map_shortcode' );
+		if ( ! empty( $parallax_one_contact_map_shortcode ) ) {
+			echo '<div class="contact-page-map-wrap">';
+			echo do_shortcode( $parallax_one_contact_map_shortcode );
+			echo '</div>';
+		}
+		?>
+	
+
+</article><!-- #post-## -->

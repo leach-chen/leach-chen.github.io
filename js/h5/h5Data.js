e158d1edf0584d0f975cbc9b56dca0a6755552de
LAPTOP-8CFJ92M4\34976 <349763031@qq.com>
2019-02-17 21:11:47 +0800

    modify

---
+++ js/h5/h5Data.js
@@ -0,0 +1,13 @@
+
+var host = "https://www.leachchen.com/h5website/"
+var h5DataList = [
+    {
+      thumb: host+"001/thumb/demo.png",
+      preview: host+"001/source/",
+      download: host+"001/download/before.zip"
+    }
+  ];
+  
+  export{
+      h5DataList
+  }

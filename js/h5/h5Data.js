3435715bdba9dc51be431148e574c96df9ca542e
leach.chen <leach>
2019-02-18 18:34:09 +0800

    modify

--- js/h5/h5Data.js
+++ js/h5/h5Data.js
@@ -1,11 +1,7 @@
 
 var host = "https://www.leachchen.com/h5website/"
 var h5DataList = [
-    {
-      thumb: host+"001/thumb/demo.png",
-      preview: host+"001/source/",
-      download: host+"001/download/before.zip"
-    }
+    {thumb: host+"001/thumb/demo.png",preview: host+"001/source/",download: host+"001/download/before.zip",description:"旅游风格的H5网站"},
   ];
   
   export{

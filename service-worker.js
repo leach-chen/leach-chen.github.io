46e49af62c43083591737c5894925a1670fd457a
leach-chen <349763031@qq.com>
2019-03-29 13:11:50 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -15,6 +15,7 @@ importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox
 
 
 
+
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
  * requests for URLs in the manifest.
